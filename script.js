6cfe14d252339cc5f82cc038310f499687b6bc73
Litafe <lifetafere@gmail.com>
2026-06-05 01:10:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 function generatePassword() {
-  let length = document.getElementById("length").value;
-  let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+  const length = parseInt(document.getElementById("length").value);
+  const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
   let password = "";
 
   for (let i = 0; i < length; i++) {
-    password += chars[Math.floor(Math.random() * chars.length)];
+    password += chars.charAt(Math.floor(Math.random() * chars.length));
   }
 
   document.getElementById("output").innerText = password;
 }
 
 function analyzePassword() {
-  let pass = document.getElementById("analyzeInput").value;
+  const pass = document.getElementById("analyzeInput").value;
 
   let score = 0;
+
   if (pass.length >= 8) score++;
   if (/[A-Z]/.test(pass)) score++;
   if (/[0-9]/.test(pass)) score++;
-  if (/[\!\@\#\$\%\^\&\*]/.test(pass)) score++;
+  if (/[!@#$%^&*]/.test(pass)) score++;
 
-  let strength = ["Weak","Medium","Strong","Very Strong"][score];
+  let strength = "";
+
+  if (score <= 1) {
+    strength = "Weak";
+  } else if (score === 2) {
+    strength = "Medium";
+  } else if (score === 3) {
+    strength = "Strong";
+  } else {
+    strength = "Very Strong";
+  }
+
   document.getElementById("strength").innerText = strength;
 }
